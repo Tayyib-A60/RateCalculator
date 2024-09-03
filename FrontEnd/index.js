4a88aa42f69bb6649ba012f1a91643c3e7ab74e9
tayyib-a60 <toyeeb.adesokan@gmail.com>
2024-09-03 21:00:34 +0100

    Updated with additional pages

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,6 +2,8 @@ let rate = 0;
 // const baseUrl = 'https://localhost:7298';
 const baseUrl = 'https://rate-calculator-1-0.onrender.com';
 
+let sendEnabled = false;
+
 async function init() {
     const getRateAPIResponse = await fetch(`${baseUrl}/api/Rate/get-rate`);
 
@@ -18,6 +20,30 @@ async function init() {
     const gbpInputs = document.getElementsByClassName('from-currency');
     const ngnInputs = document.getElementsByClassName('to-currency');
 
+    const accountNumberInputs = document.getElementsByClassName('account-number');
+
+    const sendButtons = document.getElementsByClassName('send');
+
+    if(accountNumberInputs.length) {
+        const accountNumberInput = accountNumberInputs[0];
+
+        accountNumberInput.addEventListener('input', (event) => {
+            const accountNumber = event.target.value;
+
+            if(accountNumber.length === 10) {
+                if(sendButtons.length) {
+                    const sendButton = sendButtons[0];
+                    sendButton.disabled = false;
+                }
+            } else {
+                if(sendButtons.length) {
+                    const sendButton = sendButtons[0];
+                    sendButton.disabled = true;
+                }
+            }
+        });
+    }
+
     if(gbpInputs.length && ngnInputs.length) {
         const gbpInput = gbpInputs[0];
         const ngnInput = ngnInputs[0];
@@ -31,6 +57,59 @@ async function init() {
             const gbpCalculatedValue = Number(event.target.value) / rate;
             gbpInput.value = gbpCalculatedValue.toFixed(2);
         });
+    }
+}
+
+function sendNow() {
+    const sections = document.getElementsByClassName('flex-column');
+
+    if(sections.length > 1) {
+        sections[0].classList.remove('disp-flex');
+        sections[0].classList.add('disp-none');
+        sections[1].classList.remove('disp-none');
+        sections[1].classList.add('disp-flex');
+        sections[2].classList.remove('disp-flex');
+        sections[2].classList.add('disp-none');
+    }
+}
+
+function send() {
+    const sections = document.getElementsByClassName('flex-column');
+
+    if(sections.length > 2) {
+        sections[0].classList.remove('disp-flex');
+        sections[0].classList.add('disp-none');
+        sections[1].classList.remove('disp-flex');
+        sections[1].classList.add('disp-none');
+        sections[2].classList.remove('disp-none');
+        sections[2].classList.add('disp-flex');
+    }
+
+    const accountNumberInputs = document.getElementsByClassName('account-number');
+
+    if(accountNumberInputs.length) {
+        const accountNumberInput = accountNumberInputs[0];
+        accountNumberInput.value = "";
+    }
+
+    const sendButtons = document.getElementsByClassName('send');
+
+    if(sendButtons.length) {
+        const sendButton = sendButtons[0];
+        sendButton.disabled = true;
+    }
+}
+
+function home() {
+    const sections = document.getElementsByClassName('flex-column');
+
+    if(sections.length > 2) {
+        sections[0].classList.remove('disp-none');
+        sections[0].classList.add('disp-flex');
+        sections[1].classList.remove('disp-flex');
+        sections[1].classList.add('disp-none');
+        sections[2].classList.remove('disp-flex');
+        sections[2].classList.add('disp-none');
     }
 }
 
